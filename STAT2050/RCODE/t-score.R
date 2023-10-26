426036431be02928f3972a445a75b5bcb17c83d4
Clstrange <57571396+Clstrange@users.noreply.github.com>
2023-10-26 12:06:24 -0600

    stats

--- STAT2050/RCODE/t-score.R
+++ STAT2050/RCODE/t-score.R
@@ -1,5 +1,5 @@
-# Percent confidence from T-score
+# Percent confidence or p-value from T-score or
 pt(2.1,11,lower.tail = F)
 
-# T-score from confidence interval of 90%
+# T-score from confidence interval of 90% or p-value
 qt(0.05, 49, lower.tail = F)
